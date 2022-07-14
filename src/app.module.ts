6dd3bf72a8df4e3f4ecb86c4eb0cefce1e7d3157
noveltaker <pnw123456@icloud.com>
2022-07-14 15:14:32 +0900

    build: default port setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppResource } from './web/AppResource';
 import { AppService } from './service/AppService';
 
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
   controllers: [AppResource],
   providers: [AppService],
 })
